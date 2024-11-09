f6c01d45735f9ff04ddeff546a5c3e353c206cbe
gaaldvd <gaaldvd@proton.me>
2024-11-09 18:03:31 +0100

    update script modified

--- update.sh
+++ update.sh
@@ -3,16 +3,19 @@ echo "> Python version: $(python --version)"
 echo "> PIP version: $(pip --version)"
 echo "> pipenv version: $(pipenv --version)"
 
+# prompt for git update
 until [ "$git_up" = "y" ] || [ "$git_up" = "Y" ] || [ "$git_up" = "n" ] || [ "$git_up" = "N" ]
 do
   read -r -p "Update git repository? [y/n] " git_up
 done
 
+# prompt for env update
 until [ "$pip_up" = "y" ] || [ "$pip_up" = "Y" ] || [ "$pip_up" = "n" ] || [ "$pip_up" = "N" ]
 do
   read -r -p "Update python environment? [y/n] " pip_up
 done
 
+# update git repo
 if [ "$git_up" = "y" ] || [ "$git_up" = "Y" ]
 then
   echo "> Updating git repository..."
@@ -20,6 +23,7 @@ then
 fi
 echo "> Git repository is up to date."
 
+# update env
 if [ "$pip_up" = "y" ] || [ "$pip_up" = "Y" ]
 then
   echo "> Updating python environment..."
@@ -27,7 +31,10 @@ then
 fi
 echo "> Python environment is up to date."
 
+# verify packages
 echo "> Pipfile verification... $(pipenv verify)"
+
+# show package info
 echo "> beautifulsoup4: $(pipenv run pip show beautifulsoup4)"
 echo "> progress: $(pipenv run pip show progress)"
 
